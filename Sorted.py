1289eef622e3230f71594666284a09bae7df6214
liangliang <1131980108@qq.com>
2019-01-22 09:48:47 +0800

    Update Sorted.py

--- Sorted.py
+++ Sorted.py
@@ -19,3 +19,14 @@ def quick_sorted(arr):
 
 # 归并排序
 
+def mergeSort(arr):
+    if len(arr)<=1:
+        return arr
+    middle=int(len(arr)/2)
+    left=mergeSort(arr[:middle])
+    right=mergeSort(arr[middle:])
+    merged=[]
+    while left and right:
+        merged.append(left.pop(0) if left[0]<right[0] else right.pop(0))
+    merged.extend(right if right else left)
+    return merged
